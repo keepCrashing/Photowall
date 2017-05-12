38550a2a8a945d740a24b69f60b9cb4277ff0381
keepCrashing <deer.of.nine.colors@gmail.com>
2017-05-12 14:44:51 +0800

    pod install

--- Pods/Target Support Files/Pods-Photowall-PhotowallTests/Pods-Photowall-PhotowallTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Photowall_PhotowallTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Photowall_PhotowallTestsVersionString[];
-
--- Pods/Target Support Files/Pods-Photowall/Pods-Photowall-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Photowall : NSObject
-@end
-@implementation PodsDummy_Pods_Photowall
-@end
